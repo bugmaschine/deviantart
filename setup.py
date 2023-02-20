4d71f68272cc415c94777bf669c49113a786a749
Bugmaschine <lowikian@duck.com>
2023-02-21 00:09:24 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="new-deviantart",
-    version="0.1.5",
+    version="0.1.6",
     description="A Python wrapper for the DeviantArt API",
     url="https://github.com/bugmaschine/deviantart",
     author="Kevin Eichhorn",
